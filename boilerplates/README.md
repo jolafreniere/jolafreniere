8866815fe17b99deaeb91e9d5d22ff9a05afa4ba
Jonathan Lafrenière <46546590+jolafreniere@users.noreply.github.com>
2022-08-18 17:53:15 -0400

    - Move ts boilerplate to its own repo - Fix tests

---
+++ boilerplates/README.md
@@ -0,0 +1,9 @@
+# Boilerplates
+
+This directory contains a collection of various boilerplates which I may need in the future.
+
+Boilerplates which were deemed worthy of having their own repositories are the following:
+
+## [Typescript Project Boilerplate](https://github.com/jolafreniere/boilerplate-ts)
+
+This boilerplate is used by my custom npm-init script.
--- configuration-files/.npm-init.js
+++ configuration-files/.npm-init.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
+//const fsx = require("fs-extra");
+
 // This file has to be created in the user home directory
 // It will override the default behavior of the "npm init" command
 // This will make it so that a bunch of dev dependencies are installed, a bunch of scripts are created and the github registry is set correctly
+// TODO: Use FSX to copy the directory recursively
 const boilerplateDir = require("os").homedir() + "\\npm-boilerplates";
 let pt = "js";
 
@@ -15,6 +18,7 @@ const tsBoilerplateFiles = {
   "./.gitignore": `${boilerplateDir}\\ts\\.gitignore`,
   "./rest.http": `${boilerplateDir}\\ts\\rest.http`,
   "./src/index.ts": `${boilerplateDir}\\ts\\src\\index.ts`,
+  "./src/test/index.test.ts": `${boilerplateDir}\\ts\\src\\index.ts`,
 };
 
 module.exports = {
@@ -26,7 +30,7 @@ module.exports = {
     if (answer == "ts") {
       pt = answer;
       if (!fs.existsSync("./src")) fs.mkdirSync("./src");
-      if (!fs.existsSync("./test")) fs.mkdirSync("./test");
+      if (!fs.existsSync("./src/test")) fs.mkdirSync("./src/test");
       if (!fs.existsSync("./dist")) fs.mkdirSync("./dist");
       if (!fs.existsSync("./.github")) fs.mkdirSync("./.github");
       if (!fs.existsSync("./.github/workflows"))
@@ -108,7 +112,6 @@ module.exports = {
         test: "jest --coverage",
         prettier: "prettier --config .prettierrc --write .",
         "test:watch": "jest --watch",
-        versionBump: "bump --tag --push --all",
       };
     } else {
       return {
